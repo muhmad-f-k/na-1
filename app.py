0a6c8ec1972754d9a140bd8a3ef6e3a207f349af
mikal <mikal95@live.no>
2022-02-28 15:53:01 +0100

    La til form i dinner.html og forandret oppstartsmåten i app.py til debug mode

--- app.py
+++ app.py
@@ -2,10 +2,8 @@ from flask import Flask
 
 from dinner.routes import dinner
 
+app = Flask(__name__)
+app.register_blueprint(dinner)
 
-def create_app():
-    app = Flask(__name__)
-
-    app.register_blueprint(dinner)
-
-    return app
+if __name__ == '__main__':
+    app.run(debug=True)
