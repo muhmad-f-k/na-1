5daed36cf83fb758b7447ba18dd19b212df30087
mikal <mikal95@live.no>
2022-02-28 17:00:39 +0100

    Laget form for dinner.html

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
-
 from dinner.routes import dinner
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mrbean'
 app.register_blueprint(dinner)
 
 if __name__ == '__main__':
--- dinner/routes.py
+++ dinner/routes.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template
+from dinner.forms import RegistrationForm
 
 dinner = Blueprint('dinner', __name__)
 
 
 @dinner.route('/dinner')
 def index():
-    return render_template('dinner.html')
+    form = RegistrationForm()
+    return render_template('dinner.html', form=form)
