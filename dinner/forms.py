a71105f26b04bc12938a6193f8c6aae1022ca6f6
mikal <mikal95@live.no>
2022-03-01 10:43:13 +0100

    La til filopplastning til middagsside

--- dinner/forms.py
+++ dinner/forms.py
@@ -1,9 +1,12 @@
 from flask_wtf import FlaskForm
+from  flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 
 class RegistrationForm(FlaskForm):
-    dinnerName = StringField('Username',
+    dinnerName = StringField('Navn',
                              validators=[DataRequired(), Length(min=2, max=20)])
+    image = FileField('Bilde av middag',
+                      validators=[FileAllowed(['jpg', 'png']), FileRequired()])
     submit = SubmitField('Lagre')
