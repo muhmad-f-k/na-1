38a6a5017230e0bebed32e0a2cc3ac2bfed12516
mikal <mikal95@live.no>
2022-03-01 12:16:17 +0100

    La dato til middagsside

--- dinner/forms.py
+++ dinner/forms.py
@@ -1,12 +1,13 @@
 from flask_wtf import FlaskForm
 from  flask_wtf.file import FileField, FileAllowed, FileRequired
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, DateField
 from wtforms.validators import DataRequired, Length
 
 
 class RegistrationForm(FlaskForm):
     dinnerName = StringField('Navn',
                              validators=[DataRequired(), Length(min=2, max=20)])
+    day = DateField('Dato', format='%m/%d/%Y')
     image = FileField('Bilde av middag',
                       validators=[FileAllowed(['jpg', 'png']), FileRequired()])
     submit = SubmitField('Lagre')
--- dinner/routes.py
+++ dinner/routes.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, url_for, redirect
 from dinner.forms import RegistrationForm
 
 dinner = Blueprint('dinner', __name__)
 
 
-@dinner.route('/dinner')
+@dinner.route('/dinner', methods=['GET', 'POST'])
 def index():
     form = RegistrationForm()
     return render_template('dinner.html', form=form)
