26a559c7479db027f25e3dee6f4fdad8f2e89254
mikal <mikal95@live.no>
2022-03-01 15:27:17 +0100

    Dato legges til ordentlig nå

--- dinner/forms.py
+++ dinner/forms.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
 from  flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import StringField, SubmitField, DateField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Optional
 
 
 class MakeDinnerForm(FlaskForm):
     dinnerName = StringField('Navn',
                              validators=[DataRequired(), Length(min=2, max=20)])
-    #mp_day = DateField('Dato', format='%m/%d/%Y')
+    mp_day = DateField('Dato', validators=[DataRequired()])
     image = FileField('Bilde av middag',
-                      validators=[FileAllowed(['jpg', 'png'])])
+                      validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
     submit = SubmitField('Lagre')
--- dinner/routes.py
+++ dinner/routes.py
@@ -7,9 +7,14 @@ dinner = Blueprint('dinner', __name__)
 @dinner.route('/dinner', methods=['GET', 'POST'])
 def index():
     form = MakeDinnerForm()
-    print(form.errors)
+    #print(form.errors)
+    #if form.is_submitted():
+    #    flash(f'Middag {form.dinnerName.data}')
+    #    flash(f'Bilde: {form.image.data}')
+    #    flash(f'dato: {form.mp_day.data}')
     if form.validate_on_submit():
         flash(f'Middag {form.dinnerName.data} opprettet!')
-        flash(f'Bilde: {form.image.data}')
-    print(form.errors)
+        #flash(f'Bilde: {form.image.data}')
+        #flash(f'dato: {form.mp_day.data}')
+    #print(form.errors)
     return render_template('dinner.html', form=form)
