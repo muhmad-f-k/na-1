45d0392a9c7ff0c25a74bcc1c98b06791b1d5d2e
Mkogithub <Mkogithub@users.noreply.github.com>
2022-03-05 16:32:52 +0100

    More template inheritance

--- home/routes.py
+++ home/routes.py
@@ -1,11 +1,14 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 
-home = Blueprint('simple_page', __name__, template_folder='templates')
-
-home = Blueprint('home', __name__)
+home = Blueprint('simple_page', __name__)
 
 
 @home.route('/home')
 def index():
+    return render_template('index.html')
+
+
+@home.route('/')
+def Index():
     return render_template('index.html')
