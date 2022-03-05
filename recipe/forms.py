43ea6f2802c748821f703798a252c9ff8bc1dfd7
Jonathan Skaue <jonathan@skaue.com>
2022-03-05 17:01:11 +0100

    Ordnet database og laget insertion queries

--- recipe/forms.py
+++ recipe/forms.py
@@ -7,6 +7,3 @@ class CreateRecipeForm(FlaskForm):
     recipeApproach = TextAreaField('Approach', validators=[DataRequired(), Length(min=1, max=1000)])
     recipeVersion = IntegerField('Versjon', validators=[DataRequired(), Length(min=1, max=10000)])
     submit = SubmitField('Opprett')
-
-class AddIngredientForm(FlaskForm):
-
--- recipe/routes.py
+++ recipe/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from recipe.forms import CreateRecipeForm
+from modul import *
 
 recipe = Blueprint('recipe', __name__)
 
@@ -7,8 +8,9 @@ recipe = Blueprint('recipe', __name__)
 @recipe.route('/create_recipe', methods=['GET', 'POST'])
 def createRecipe():
     form = CreateRecipeForm()
+    dinner_id = 1
     if form.validate_on_submit():
-        recipe = Recipe(approach=form.recipeApproach, version=form.recipeVersion, dinner_id = dinner_id)
+        recipe = Recipe(approach=form.recipeApproach, version=form.recipeVersion, dinner_id=dinner_id)
         session.add(recipe)
         session.commit()
     return render_template('recipe.html', form=form)
