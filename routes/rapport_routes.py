b5e380e9276b9899f6cc35af0faabae872995e33
Muhmad K. Fauzi <muhmad@live.no>
2022-04-01 12:29:49 +0200

    report

--- routes/rapport_routes.py
+++ routes/rapport_routes.py
@@ -32,13 +32,13 @@ def report_post():
     top_3_dinner_result = count.most_common(3)
 # husk å endre til å ta gruppe id
     ingredient = session.query(Ingredient.name).select_from(
-        Recipe).join(Recipe_ingredient_helper).join(Measurement).join(Ingredient).join(Amount).filter(Meal.date.between(start_date, end_date), Meal.group_id == 1).all()
+        Recipe).join(Recipe_ingredient_helper).join(Measurement).join(Ingredient).join(Amount).filter(Meal.date.between(start_date, end_date), Meal.dinner_id == 1).all()
 
     measurement = session.query(Measurement.name).select_from(
-        Recipe).join(Recipe_ingredient_helper).join(Measurement).join(Ingredient).join(Amount).filter(Meal.date.between(start_date, end_date), Meal.group_id == 1).all()
+        Recipe).join(Recipe_ingredient_helper).join(Measurement).join(Ingredient).join(Amount).filter(Meal.date.between(start_date, end_date), Meal.dinner_id == 1).all()
 
     amount = session.query(Amount.amount).select_from(
-        Recipe).join(Recipe_ingredient_helper).join(Measurement).join(Ingredient).join(Amount).filter(Meal.date.between(start_date, end_date), Meal.group_id == 1).all()
+        Recipe).join(Recipe_ingredient_helper).join(Measurement).join(Ingredient).join(Amount).filter(Meal.date.between(start_date, end_date), Meal.dinner_id == 1).all()
 
     print(ingredient)
     print(measurement)
@@ -46,7 +46,7 @@ def report_post():
     return render_template("report_result.html", cost=total_price, top_3_dinner=top_3_dinner_result, data=ingredient, data1=measurement, data2=amount)
 
 
-@rapportroute.route("/rapport_ingredient", methods=['POST'])
+@ rapportroute.route("/rapport_ingredient", methods=['POST'])
 def rapport_ingredient_post():
     user = current_user
     start_date = request.form.get("start_date")
