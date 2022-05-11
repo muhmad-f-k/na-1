946d9dc529cdbf2f1bfecf71ec64dbb4d0b2e4d0
Muhmad <muhmad@live.no>
2022-05-11 12:30:02 +0200

    Update queries.py

--- queries.py
+++ queries.py
@@ -1,4 +1,5 @@
-""" from db.modul import *
+from db.modul import *
+from sqlalchemy import desc
 
 
 def get_user_by_email(email):
@@ -47,4 +48,27 @@ def save_role_group(user_id, group_id, role_id):
     session.commit()
     session.close()
     return add_role_group
- """
+
+
+def get_ingredient_by_name(name):
+    return session.query(Ingredient).filter(Ingredient.name == name).first()
+
+
+def get_amount_by_amount(amount):
+    return session.query(Amount).filter(Amount.amount == amount).first()
+
+
+def get_measurement_by_name(name):
+    return session.query(Measurement).filter(Measurement.name == name).first()
+
+
+def get_recipe_with_dinner_id(dinner_id):
+    return session.query(Recipe).filter(Recipe.dinner_id == dinner_id).order_by(desc(Recipe.version)).first()
+
+
+def get_approach_by_approach(approach):
+    return session.query(Recipe).filter(Recipe.approach == approach).first()
+
+
+def check_recipe_ingredient_helper_by_recipe_id(recipe_id):
+    return session.query(Recipe_ingredient_helper).filter(Recipe_ingredient_helper.recipe_id == recipe_id).order_by(desc(Recipe.version)).first()
