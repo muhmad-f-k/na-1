a0322b7f239776753cccd08e0064b8f860cb5eb0
mko095 <mko095@post.uit.no>
2022-05-11 18:10:19 +0200

    maga har lakt inn mye bra

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -118,9 +118,13 @@ def privacy():
     return render_template("/info/privacy.html")
 
 
-@usersroute.route('/terms_of_use')
+@usersroute.route('/Vilkår_for_bruk')
 def terms_of_use():
-    return render_template("/info/terms_of_use")
+    return render_template("/info/terms_of_use.html")
+
+@usersroute.route('/Bruksanvisning')
+def instructions_for_use():
+    return render_template("/info/instructions_for_use.html")
 
 
 @usersroute.route('/logout')
